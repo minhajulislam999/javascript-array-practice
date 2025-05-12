581d29cc6913caa678a39c1f320f31d544ad9afc
minhajul islam <mm01882390860@gmail.com>
2025-05-12 11:45:13 +0600

    Added array practice code with fruits and countries

---
+++ script/script.js
@@ -0,0 +1,17 @@
+const countries = ["Bangladesh", "Pakistan", "Afghanistan"];
+
+
+console.log(countries[0]);
+
+
+const fruits = ["apple", "orange", "lemon", "grape", "banana"];
+
+fruits[2] = "pineapple";
+
+fruits.push("watermelon");
+
+console.log(fruits);
+
+
+
+
